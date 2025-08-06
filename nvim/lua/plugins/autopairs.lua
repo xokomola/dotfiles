957aefac1b1e71525d6c4182986c2bcfa774a0ed
Marc van Grootel <marc.van.grootel@gmail.com>
2025-08-06 13:42:51 +0200

    Add kickstart stuff

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,9 @@
+-- Autopairs is a Neovim plugin that automatically closes pairs of characters like
+-- parentheses, brackets, and quotes.
+-- https://github.com/windwp/nvim-autopairs
+
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  opts = {},
+}
---
+++ nvim/lua/plugins/indent_line.lua
@@ -0,0 +1,13 @@
+-- Indent Blankline is a Neovim plugin that adds indentation guides to your 
+-- code.
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+
+return {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+}
